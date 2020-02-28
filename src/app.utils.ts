e249bc1387a015a452e3793415b9afdc1ca7ebbb
Harijs Deksnis <harijs.deksnis@gmail.com>
2020-02-28 10:15:52 +0100

    add more sample data

--- src/app.utils.ts
+++ src/app.utils.ts
@@ -25,6 +25,35 @@ export const MOVIES: Movie[] = [
     directors: [],
     writers: [],
   },
+  {
+    id: uuidv4(),
+    name: 'Inglourious Basterds',
+    year: 2009,
+    plot: 'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. ' +
+      'soldiers coincides with a theatre owner\'s vengeful plans for the same.',
+    actors: [],
+    directors: [],
+    writers: [],
+  },
+  {
+    id: uuidv4(),
+    name: 'Django Unchained',
+    year: 2012,
+    plot: 'With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.',
+    actors: [],
+    directors: [],
+    writers: [],
+  },
+  {
+    id: uuidv4(),
+    name: 'Inception',
+    year: 2010,
+    plot: 'A thief who steals corporate secrets through the use of dream-sharing technology is ' +
+      'given the inverse task of planting an idea into the mind of a C.E.O.',
+    actors: [],
+    directors: [],
+    writers: [],
+  },
 ];
 
 export const ACTORS: Actor[] = [
@@ -70,10 +99,55 @@ export const ACTORS: Actor[] = [
     birthYear: 1966,
     nationality: 'American',
   },
+  {
+    id: uuidv4(),
+    name: 'Christoph Waltz',
+    birthYear: 1956,
+    nationality: 'Austrian',
+  },
+  {
+    id: uuidv4(),
+    name: 'Michael Fassbender',
+    birthYear: 1977,
+    nationality: 'British',
+  },
+  {
+    id: uuidv4(),
+    name: 'Jamie Foxx',
+    birthYear: 1967,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Leonardo DiCaprio',
+    birthYear: 1974,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Joseph Gordon-Levitt',
+    birthYear: 1981,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Ellen Page',
+    birthYear: 1987,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Tom Hardy',
+    birthYear: 1977,
+    nationality: 'British',
+  },
 ];
 
 MOVIES[0].actors = [ACTORS[0].id, ACTORS[1].id, ACTORS[2].id, ACTORS[3].id];
 MOVIES[1].actors = [ACTORS[4].id, ACTORS[5].id, ACTORS[6].id];
+MOVIES[2].actors = [ACTORS[5].id, ACTORS[7].id, ACTORS[8].id];
+MOVIES[3].actors = [ACTORS[7].id, ACTORS[9].id, ACTORS[10].id];
+MOVIES[4].actors = [ACTORS[10].id, ACTORS[11].id, ACTORS[12].id, ACTORS[13].id];
 
 export const DIRECTORS = [
   {
@@ -94,10 +168,25 @@ export const DIRECTORS = [
     birthYear: 1962,
     nationality: 'American',
   },
+  {
+    id: uuidv4(),
+    name: 'Quentin Tarantino',
+    birthYear: 1963,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Christopher Nolan',
+    birthYear: 1970,
+    nationality: 'American',
+  },
 ];
 
 MOVIES[0].directors = [DIRECTORS[0].id, DIRECTORS[1].id];
 MOVIES[1].directors = [DIRECTORS[2].id];
+MOVIES[2].directors = [DIRECTORS[3].id];
+MOVIES[3].directors = [DIRECTORS[3].id];
+MOVIES[4].directors = [DIRECTORS[4].id];
 
 export const WRITERS = [
   {
@@ -124,7 +213,22 @@ export const WRITERS = [
     birthYear: 1957,
     nationality: 'American',
   },
+  {
+    id: uuidv4(),
+    name: 'Quentin Tarantino',
+    birthYear: 1963,
+    nationality: 'American',
+  },
+  {
+    id: uuidv4(),
+    name: 'Christopher Nolan',
+    birthYear: 1970,
+    nationality: 'American',
+  },
 ];
 
 MOVIES[0].writers = [WRITERS[0].id, WRITERS[1].id];
 MOVIES[1].writers = [WRITERS[2].id, WRITERS[3].id];
+MOVIES[2].writers = [WRITERS[4].id];
+MOVIES[3].writers = [WRITERS[4].id];
+MOVIES[4].writers = [WRITERS[5].id];
