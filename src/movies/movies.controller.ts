96898b488b2eed738fa4ad93c69192f8ef35a47c
Harijs Deksnis <harijs.deksnis@gmail.com>
2020-02-28 11:03:09 +0100

    CRUD movie

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Post, Body, BadRequestException, Delete, Put } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import { Movie } from '../app.model';
 
 @Controller('movies')
 export class MoviesController {
@@ -10,12 +11,45 @@ export class MoviesController {
     return this.moviesService.getMovies();
   }
 
+  @Post()
+  addMovie(@Body() movie: Partial<Movie>) {
+    const result = this.moviesService.addMovie(movie);
+
+    if (result) {
+      return result;
+    } else {
+      throw new BadRequestException();
+    }
+  }
+
   @Get(':id')
   getMovie(@Param('id') id: string) {
     const movie = this.moviesService.getMovie(id);
 
     if (movie) {
       return movie;
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  @Put(':id')
+  updateMovie(@Param('id') id: string, @Body() movie: Partial<Movie>) {
+    const result = this.moviesService.updateMovie(id, movie);
+
+    if (result) {
+      return result;
+    } else {
+      throw new BadRequestException();
+    }
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    const result = this.moviesService.deleteMovie(id);
+
+    if (result) {
+      return;
     } else {
       throw new NotFoundException();
     }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,6 @@
 import { pick, find } from 'lodash';
 import { Injectable } from '@nestjs/common';
-import { MOVIES } from '../app.utils';
+import { MOVIES, uuidv4 } from '../app.utils';
 import { Movie } from '../app.model';
 
 @Injectable()
@@ -17,6 +17,48 @@ export class MoviesService {
 
   getMovie(id: string) {
     return find(this.movies, { id });
+  }
+
+  addMovie({ name, plot, year }: Partial<Movie>) {
+    let result;
+
+    if (name && plot && year) {
+      result = {
+        id: uuidv4(),
+        name,
+        plot,
+        year,
+        actors: [],
+        directors: [],
+        writers: [],
+      };
+      this.movies.push(result);
+    }
+
+    return result;
+  }
+
+  updateMovie(id: string, { name, plot, year }: Partial<Movie>) {
+    const movie = find(this.movies, { id });
+    console.log('update', { name, plot, year })
+    if (name && plot && year && movie) {
+      movie.name = name;
+      movie.plot = plot;
+      movie.year = year;
+    }
+
+    return movie;
+  }
+
+  deleteMovie(id: string) {
+    const movie = find(this.movies, { id });
+
+    if (movie) {
+      this.movies = this.movies.filter(({ id: movieId }) => movieId !== id);
+      return true;
+    } else {
+      return;
+    }
   }
 
 }
