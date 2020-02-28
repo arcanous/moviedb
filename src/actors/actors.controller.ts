e75abe6bb6e8f983e0680cea6b598b94e06092e6
Harijs Deksnis <harijs.deksnis@gmail.com>
2020-02-28 11:46:21 +0100

    actors CUD

--- src/actors/actors.controller.ts
+++ src/actors/actors.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { Actor } from './../app.model';
+import { Controller, Get, Param, NotFoundException, Put, Delete, BadRequestException, Body, Post } from '@nestjs/common';
 import { ActorsService } from './actors.service';
 
 @Controller('actors')
@@ -16,6 +17,39 @@ export class ActorsController {
 
     if (actor) {
       return actor;
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  @Post()
+  addActor(@Body() actor: Partial<Actor>) {
+    const result = this.actorsService.addActor(actor);
+
+    if (result) {
+      return result;
+    } else {
+      throw new BadRequestException();
+    }
+  }
+
+  @Put(':id')
+  updateActor(@Param('id') id: string, @Body() actor: Partial<Actor>) {
+    const result = this.actorsService.updateActor(id, actor);
+
+    if (result) {
+      return result;
+    } else {
+      throw new BadRequestException();
+    }
+  }
+
+  @Delete(':id')
+  deleteActor(@Param('id') id: string) {
+    const result = this.actorsService.deleteActor(id);
+
+    if (result) {
+      return;
     } else {
       throw new NotFoundException();
     }
--- src/actors/actors.service.ts
+++ src/actors/actors.service.ts
@@ -1,6 +1,6 @@
 import { find } from 'lodash';
 import { Injectable } from '@nestjs/common';
-import { ACTORS, MOVIES } from './../app.utils';
+import { ACTORS, MOVIES, uuidv4 } from './../app.utils';
 import { Actor } from './../app.model';
 
 @Injectable()
@@ -23,6 +23,48 @@ export class ActorsService {
         ...actor,
         movies: MOVIES.filter(({ actors }) => actors.includes(id)).map(({ id: movieId }) => movieId),
       };
+    } else {
+      return;
+    }
+  }
+
+  addActor({ name, birthYear, nationality }: Partial<Actor>) {
+    let result;
+
+    if (name && birthYear && nationality) {
+      result = {
+        id: uuidv4(),
+        name,
+        birthYear,
+        nationality,
+      };
+      this.actors.push(result);
+    }
+
+    return result;
+  }
+
+  updateActor(id: string, { name, birthYear, nationality }: Partial<Actor>) {
+    const actor = find(this.actors, { id });
+
+    if (name && birthYear && nationality && actor) {
+      actor.name = name;
+      actor.birthYear = birthYear;
+      actor.nationality = nationality;
+    }
+
+    return actor;
+  }
+
+  deleteActor(id: string) {
+    const actor = find(this.actors, { id });
+
+    if (actor) {
+      this.actors = this.actors.filter(({ id: actorId }) => actorId !== id);
+      MOVIES.forEach(movie => {
+        movie.actors = movie.actors.filter(actorId => actorId !== id);
+      });
+      return true;
     } else {
       return;
     }
